1400093c39657d6c99b5aa7475c31bf706b08c7b
Lordex <130374125+LordexDuck3990@users.noreply.github.com>
2024-01-05 23:46:21 +0200

    Make drawText.ts

--- src/functions/drawText.ts
+++ src/functions/drawText.ts
@@ -1,1 +1,63 @@
-hi
+import { AttachmentBuilder } from "discord.js"
+import { ArgType, NativeFunction } from "forgescript"
+import CanvasBuilder from "../classes/builder"
+
+export default new NativeFunction({
+    name: "$drawText",
+    version: "1.0.0",
+    description: "Draws text in provided canvas.",
+    unwrap: true,
+    brackets: true,
+    args: [
+        {
+            name: "canvas",
+            description: "The name of canvas to draw text on.",
+            rest: false,
+            type: ArgType.String,
+            required: true
+        },
+        {
+            name: "text",
+            description: "The text to draw.",
+            rest: false,
+            type: ArgType.String,
+            required: true
+        },
+        {
+            name: "x",
+            description: "The X position of text.",
+            rest: false,
+            type: ArgType.Number,
+            required: true
+        },
+        {
+            name: "y",
+            description: "The Y position of text.",
+            rest: false,
+            type: ArgType.Number,
+            required: true
+        },
+        {
+            name: "font",
+            description: "The text font.",
+            rest: false,
+            type: ArgType.String,
+            required: true
+        },
+        {
+            name: "color",
+            description: "The text color.",
+            rest: false,
+            type: ArgType.Number,
+            required: false
+        }
+    ],
+    execute(ctx, [canvas, x, y, text, font, color]) {
+        if (!ctx.canvases || !ctx.canvases[canvas] || !(ctx.canvases[canvas] instanceof CanvasBuilder))
+          return this.customError("No canvas with provided name.");
+
+        ctx.canvases[name].drawText(text, x, y, font, color)
+
+        return this.success()
+    },
+})
