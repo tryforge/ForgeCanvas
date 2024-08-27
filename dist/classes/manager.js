444cc97b202aea82b4802d770968ca1b5b7160e0
Lordex <130374125+devlordduck@users.noreply.github.com>
2024-08-27 15:50:36 +0300

    update dist

--- dist/classes/manager.js
+++ dist/classes/manager.js
@@ -1,12 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CanvasManager = void 0;
+exports.GradientManager = exports.CanvasManager = void 0;
+const canvas_1 = require("@napi-rs/canvas");
 const builder_1 = require("./builder");
-class CanvasManager {
+const typings_1 = require("../typings");
+class Manager {
     map;
-    current;
     constructor() {
         this.map = new Map();
+    }
+    ;
+    get(name) { return this.map.get(name); }
+    ;
+    remove(name) { this.map.delete(name); }
+    ;
+}
+;
+class CanvasManager extends Manager {
+    current;
+    constructor() {
+        super();
         this.current = [];
     }
     ;
@@ -17,11 +30,30 @@ class CanvasManager {
             this.map.set(name, new builder_1.CanvasBuilder(a, b ?? a));
     }
     ;
-    get(name) { return this.map.get(name); }
-    ;
-    remove(name) { this.map.delete(name); }
-    ;
 }
 exports.CanvasManager = CanvasManager;
+;
+class GradientManager extends Manager {
+    ctx;
+    stops;
+    constructor() {
+        super();
+        this.ctx = (0, canvas_1.createCanvas)(1, 1).getContext('2d');
+        this.stops = [];
+    }
+    ;
+    set(name, a, ...options) {
+        if (typings_1.GradientType?.[a])
+            this.map.set(name, a === typings_1.GradientType.radial
+                ? this.ctx.createRadialGradient(...options)
+                : a === typings_1.GradientType.conic
+                    ? this.ctx.createConicGradient(...options)
+                    : this.ctx.createLinearGradient(...options));
+        else
+            this.map.set(name, a);
+    }
+    ;
+}
+exports.GradientManager = GradientManager;
 ;
 //# sourceMappingURL=manager.js.map
