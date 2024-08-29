824ec74964eb58ee7292959b1151b82d09cc115a
Lordex <130374125+devlordduck@users.noreply.github.com>
2024-08-29 11:24:14 +0300

    create line functions

---
+++ dist/functions/canvas/lineDash.d.ts
@@ -0,0 +1,16 @@
+import { NativeFunction, ArgType } from '@tryforge/forgescript';
+declare const _default: NativeFunction<[{
+    name: string;
+    description: string;
+    type: ArgType.String;
+    required: false;
+    rest: false;
+}, {
+    name: string;
+    description: string;
+    type: ArgType.Json;
+    required: false;
+    rest: false;
+}], true>;
+export default _default;
+//# sourceMappingURL=lineDash.d.ts.map
