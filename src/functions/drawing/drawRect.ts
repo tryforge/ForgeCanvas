130816fd7660fec298ca0422b0e2d7d7ce34d886
Lordex <130374125+devlordduck@users.noreply.github.com>
2024-09-07 17:29:06 +0300

    update functions (add style argument)

--- src/functions/drawing/drawRect.ts
+++ src/functions/drawing/drawRect.ts
@@ -1,5 +1,6 @@
 import { NativeFunction, ArgType } from '@tryforge/forgescript';
-import { Context, FillOrStrokeOrClear } from '../..';
+import { Image, loadImage } from '@napi-rs/canvas';
+import { CanvasUtil, Colors, Context, FillOrStrokeOrClear, hexRegex, rgbaRegex } from '../..';
 
 export default new NativeFunction({
     name: '$drawRect',
@@ -21,6 +22,13 @@ export default new NativeFunction({
             type: ArgType.Enum,
             enum: FillOrStrokeOrClear,
             required: true,
+            rest: false
+        },
+        {
+            name: 'style',
+            description: 'The style. (color/gradient/pattern)',
+            type: ArgType.String,
+            required: false,
             rest: false
         },
         {
@@ -59,7 +67,7 @@ export default new NativeFunction({
             rest: false
         }
     ],
-    async execute (ctx: Context, [name, t, x, y, w, h, r]) {
+    async execute (ctx: Context, [name, t, style, x, y, w, h, r]) {
         const canvas = name
             ? ctx.canvasManager?.get(name)
                 : !name && ctx.canvasManager?.current?.length !== 0 
@@ -68,7 +76,60 @@ export default new NativeFunction({
         if (!canvas)
             return this.customError('No canvas');
 
+        if (!style && (t === FillOrStrokeOrClear.fill || t === FillOrStrokeOrClear.stroke))
+            return this.customError('No style provided.');
+
+        let s: any;
+        console.log(style);
+        if (style) {
+            s = style.split('://');
+            console.log(s);
+            if (s[0] === 'gradient') {
+                const gradient = ctx.gradientManager?.get(s.slice(1).join('://'));
+                if (!gradient) return this.customError('No gradient');
+
+                s = gradient;
+            } else if (s[0] === 'pattern') {
+                const splits = s.slice(1).join('://').split(':'),
+                    type = splits.shift()?.toLowerCase(),
+                    repeat = splits.length > 0 && ['repeat', 'repeat-x', 'repeat-y', 'no-repeat'].includes(splits[splits.length - 1]) ? splits.pop() : null;
+                let image: Image | ImageData;
+                console.log(splits, type, repeat, s.slice(1).join('://').split(':'), (repeat ? splits.join(':') : splits.join()));
+            
+                if (type === 'canvas') {
+                    const canvas_2 = ctx.canvasManager?.get(repeat ? splits.join(':') : splits.join())?.ctx;
+            
+                    if (!canvas_2)
+                        return this.customError('No canvas with provided name found. (pattern)');
+            
+                    image = canvas_2.getImageData(0, 0, canvas_2.canvas.width, canvas_2.canvas.height);
+                } else if (type === 'image')
+                    image = await loadImage(repeat ? splits.join(':') : splits.join(), { maxRedirects: 30 });
+                else return this.customError('Invalid pattern type.');
+
+                s = canvas.ctx.createPattern(image, repeat);
+            } else {
+                s = hexRegex.test(style) ? style 
+                    : (rgbaRegex.test(style) ? (() => {
+                        const match = style.match(rgbaRegex) as RegExpMatchArray;
+                        return CanvasUtil.rgbaToHex(
+                            parseInt(match[1], 10),
+                            parseInt(match[2], 10),
+                            parseInt(match[3], 10),
+                            match[5] ? parseFloat(match[5]) : undefined
+                        );
+                    })() : Colors[style]);
+            };
+            console.log(s);
+        }
+
+        const styleT = t === FillOrStrokeOrClear.fill ? 'fillStyle' : 'strokeStyle',
+              oldstyle = canvas.ctx[styleT];
+
+        canvas.ctx[styleT] = s ?? '#000000';
         canvas.rect(t, x, y, w, h, r);
+        canvas.ctx[styleT] = oldstyle;
+
         return this.success();
     }
 });
--- src/functions/drawing/drawText.ts
+++ src/functions/drawing/drawText.ts
@@ -1,5 +1,6 @@
 import { NativeFunction, ArgType } from '@tryforge/forgescript';
-import { CanvasUtil, Context, FillOrStroke } from '../..';
+import { Image, loadImage } from '@napi-rs/canvas';
+import { CanvasUtil, Colors, Context, FillOrStroke, hexRegex, rgbaRegex } from '../..';
 
 export default new NativeFunction({
     name: '$drawText',
@@ -35,6 +36,13 @@ export default new NativeFunction({
             description: 'The font text.',
             type: ArgType.String,
             check: (i: string) => CanvasUtil.isValidFont(i),
+            required: true,
+            rest: false
+        },
+        {
+            name: 'style',
+            description: 'The style. (color/gradient/pattern)',
+            type: ArgType.String,
             required: true,
             rest: false
         },
@@ -81,7 +89,7 @@ export default new NativeFunction({
             rest: false
         },
     ],
-    async execute (ctx: Context, [name, t, text, font, x, y, maxWidth, multiline, wrap, lineOffset]) {
+    async execute (ctx: Context, [name, t, text, font, style, x, y, maxWidth, multiline, wrap, lineOffset]) {
         const canvas = name
             ? ctx.canvasManager?.get(name)
                 : !name && ctx.canvasManager?.current?.length !== 0 
@@ -90,6 +98,47 @@ export default new NativeFunction({
         if (!canvas)
             return this.customError('No canvas');
 
+        let s: any = style.split(':');
+        if (s[0] === 'gradient' && s[1] === '//') {
+            const gradient = ctx.gradientManager?.get(s.slice(2).join(':'));
+            if (!gradient) return this.customError('No gradient');
+
+            s = gradient;
+        } else if (s[0] === 'pattern' && s[1] === '//') {
+            const splits = s.slice(2),
+                  type = splits.shift()?.toLowerCase(),
+                  repeat = splits.length > 0 && ['repeat', 'repeat-x', 'repeat-y', 'no-repeat'].includes(splits[splits.length - 1]) ? splits.pop() : null;
+            let image: Image | ImageData;
+        
+            if (type === 'canvas') {
+                const canvas_2 = ctx.canvasManager?.get(repeat ? splits.join(':') : splits.join())?.ctx;
+        
+                if (!canvas_2)
+                    return this.customError('No canvas with provided name found. (pattern)');
+        
+                image = canvas_2.getImageData(0, 0, canvas_2.canvas.width, canvas_2.canvas.height);
+            } else if (type === 'image')
+                image = await loadImage(repeat ? splits.join(':') : splits.join(), { maxRedirects: 30 });
+            else return this.customError('Invalid pattern type.');
+
+            s = canvas.ctx.createPattern(image, repeat);
+        } else {
+            s = hexRegex.test(style) ? style 
+                : (rgbaRegex.test(style) ? (() => {
+                    const match = style.match(rgbaRegex) as RegExpMatchArray;
+                    return CanvasUtil.rgbaToHex(
+                        parseInt(match[1], 10),
+                        parseInt(match[2], 10),
+                        parseInt(match[3], 10),
+                        match[5] ? parseFloat(match[5]) : undefined
+                    );
+                })() : Colors[style]);
+        };
+
+        const styleT = t === FillOrStroke.fill ? 'fillStyle' : 'strokeStyle',
+              oldstyle = canvas.ctx[styleT];
+
+        canvas.ctx[styleT] = s;
         canvas.text(
             t,
             text,
@@ -101,6 +150,8 @@ export default new NativeFunction({
             wrap,
             lineOffset
         );
+        canvas.ctx[styleT] = oldstyle;
+        
         return this.success();
     }
 });
