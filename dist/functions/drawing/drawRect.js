bc1044eeaeed98c841eab887eff8460e492dc50a
Lordex <130374125+devlordduck@users.noreply.github.com>
2024-10-10 23:43:56 +0300

    add image manager "custom protocol" (images://name)

--- dist/functions/drawing/drawRect.js
+++ dist/functions/drawing/drawRect.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const forgescript_1 = require("@tryforge/forgescript");
-const canvas_1 = require("@napi-rs/canvas");
 const __1 = require("../..");
 exports.default = new forgescript_1.NativeFunction({
     name: '$drawRect',
@@ -77,41 +76,8 @@ exports.default = new forgescript_1.NativeFunction({
             return this.customError('No canvas');
         if (!style && (t === __1.FillOrStrokeOrClear.fill || t === __1.FillOrStrokeOrClear.stroke))
             return this.customError('No style provided.');
-        let s;
-        if (style) {
-            s = style.split('://');
-            if (s[0] === 'gradient') {
-                const gradient = ctx.gradientManager?.get(s.slice(1).join('://'));
-                if (!gradient)
-                    return this.customError('No gradient');
-                s = gradient;
-            }
-            else if (s[0] === 'pattern') {
-                const splits = s.slice(1).join('://').split(':'), type = splits.shift()?.toLowerCase(), repeat = splits.length > 0 && ['repeat', 'repeat-x', 'repeat-y', 'no-repeat'].includes(splits[splits.length - 1]) ? splits.pop() : null;
-                let image;
-                if (type === 'canvas') {
-                    const canvas_2 = ctx.canvasManager?.get(repeat ? splits.join(':') : splits.join())?.ctx;
-                    if (!canvas_2)
-                        return this.customError('No canvas with provided name found. (pattern)');
-                    image = canvas_2.getImageData(0, 0, canvas_2.canvas.width, canvas_2.canvas.height);
-                }
-                else if (type === 'image')
-                    image = await (0, canvas_1.loadImage)(repeat ? splits.join(':') : splits.join(), { maxRedirects: 30 });
-                else
-                    return this.customError('Invalid pattern type.');
-                s = canvas.ctx.createPattern(image, repeat);
-            }
-            else {
-                s = __1.hexRegex.test(style) ? style
-                    : (__1.rgbaRegex.test(style) ? (() => {
-                        const match = style.match(__1.rgbaRegex);
-                        return __1.CanvasUtil.rgbaToHex(parseInt(match[1], 10), parseInt(match[2], 10), parseInt(match[3], 10), match[5] ? parseFloat(match[5]) : undefined);
-                    })() : __1.Colors[style]);
-            }
-            ;
-        }
         const styleT = t === __1.FillOrStrokeOrClear.fill ? 'fillStyle' : 'strokeStyle', oldstyle = canvas.ctx[styleT];
-        canvas.ctx[styleT] = s ?? '#000000';
+        canvas.ctx[styleT] = await __1.CanvasUtil.parseStyle(this, ctx, canvas, style) ?? '#000000';
         canvas.rect(t, x, y, w, h, r.length === 1 ? r[0] : r);
         canvas.ctx[styleT] = oldstyle;
         return this.success();
