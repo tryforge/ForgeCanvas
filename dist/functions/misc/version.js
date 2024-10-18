fe1922f2e7778b6fe9b2f44eb9394c964a2bb47c
Project Econome <108093514+Project-Econome@users.noreply.github.com>
2024-10-18 18:07:05 +0000

    test

---
+++ dist/functions/misc/version.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const forgescript_1 = require("@tryforge/forgescript");
+const package_json_1 = require("../../../package.json");
+exports.default = new forgescript_1.NativeFunction({
+    name: '$canvasVersion',
+    description: 'Returns the aoi.canvas version.',
+    version: '1.0.0',
+    brackets: true,
+    unwrap: true,
+    args: [],
+    async execute(ctx) {
+        return this.success(package_json_1.version);
+    }
+});
+//# sourceMappingURL=version.js.map
