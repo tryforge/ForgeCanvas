5101fc0d880f2aa3ab30fea611f8d209e09d8cf8
Lordex <130374125+devlordduck@users.noreply.github.com>
2025-01-13 23:25:27 +0200

    add some src handlers (rgba, rgb, hex, canvas, frame)

--- src/functions/util/loadImage.ts
+++ src/functions/util/loadImage.ts
@@ -1,6 +1,6 @@
 import { NativeFunction, ArgType } from '@tryforge/forgescript';
-import { ImageManager, Context } from '../..';
-import { loadImage } from '@napi-rs/canvas';
+import { ImageManager, Context, CanvasUtil } from '../..';
+import { createCanvas, Image, loadImage } from '@napi-rs/canvas';
 
 export default new NativeFunction({
     name: '$loadImage',
@@ -18,18 +18,76 @@ export default new NativeFunction({
             rest: false
         },
         {
-            name: 'path',
-            description: 'The image path.',
+            name: 'src',
+            description: 'The image source.',
             type: ArgType.String,
             required: true,
             rest: false
         }
     ],
-    async execute (ctx: Context, [name, path]) {
+    async execute (ctx: Context, [name, src]) {
         if (!ctx.imageManager || !(ctx.imageManager instanceof ImageManager))
             ctx.imageManager = new ImageManager();
 
-        ctx.imageManager.set(name, await loadImage(path));
+        let source;
+        if (['rgba://', 'rgb://', 'hex://'].find(x => src.startsWith(x))) {
+            const [size, data] = parseArgs(
+                src, src.startsWith('hex://') || src.startsWith('rgb://')
+                    ? 'hex://' : 'rgba://', 2
+            );
+            const [width, height] = size.split('x').map(Number);
+            
+            const canvas = createCanvas(width, height);
+            const context = canvas.getContext('2d');
+            const imageData = context.createImageData(width, height);
+
+            imageData.data.set(new Uint8ClampedArray(
+                src.startsWith('hex://')
+                    ? data.split(',').flatMap(hex => {
+                        const rgba = CanvasUtil.hexToRgba(hex.trim());
+                        return [rgba.red, rgba.green, rgba.blue, rgba.alpha ?? 255];
+                    })
+                : src.startsWith('rgb://')
+                    ? data.split(',').map(Number).flatMap((v, i) => {
+                        if ((i + 1) % 3 === 0)
+                            return [v, 255];
+                        return [v];
+                    })
+                : data.split(',').map(Number)
+            )); // :(
+
+            context.putImageData(imageData, 0, 0);
+            source = canvas.toBuffer('image/png');
+        } else if (src.startsWith('canvas://')) {
+            const canvas = ctx.canvasManager?.get(src.slice(9));
+            if (!canvas) return this.customError('Invalid canvas');
+            source = canvas.buffer;
+        } else if (src.startsWith('frame://')) {
+            const frame = ctx.gifManager?.getFrame(src.slice(8));
+            if (!frame) return this.customError('No frame');
+            const { width, height, buffer } = frame;
+
+            const canvas = createCanvas(width, height);
+            const context = canvas.getContext('2d');
+            const imageData = context.createImageData(width, height);
+
+            imageData.data.set(buffer);
+            context.putImageData(imageData, 0, 0);
+            
+            source = canvas.toBuffer('image/png');
+        } else source = src;
+
+        if (!source) return this.customError('Invalid source');
+
+        ctx.imageManager.set(name, await loadImage(source));
         return this.success();
     }
 });
+
+function parseArgs(str: string, prefix: string, length: number) {
+    const args = str.slice(prefix.length).split(':');
+    if (args.length !== length)
+        throw new Error(`${prefix} frame expects ${length} arguments.`);
+
+    return args;
+};
