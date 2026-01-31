86eec7e7f477b6a25de292506af490dbe9c06a27
Lordex <lordex@duck.com>
2026-01-31 18:19:18 +0200

    add lottie functions

--- src/functions/lottie/lottieOption.ts
+++ src/functions/lottie/lottieOption.ts
@@ -1,3 +1,8 @@
+/*
+* SPDX-License-Identifier: LGPL-3.0-or-later
+* Copyright © 2026 BotForge
+*/
+
 import { NativeFunction, ArgType } from '@tryforge/forgescript';
 import { FCError, LottieOption } from '../..';
 
